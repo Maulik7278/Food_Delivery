6da56e7e484380ee4e8a67a0e87114123fcb2a24
Jashwanth M K <160517348+Jashwanthmk@users.noreply.github.com>
2025-04-24 17:13:36 +0530

    Add files via upload

---
+++ frontend - Copy/src/App.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Footer from './components/Footer/Footer'
+import LoginPopus from './components/LoginPopus/LoginPopus'
+import Verify from './pages/Verify/Verify'
+import MyOrders from './pages/MyOrders/MyOrders'
+import AboutUs from './pages/AboutUs/AboutUs'
+
+const App = () => {
+  const [showLogin,setShowLogin]=useState(false);
+  return (
+    <>
+    {showLogin && <LoginPopus setShowLogin={setShowLogin} />}
+    <div className='app'>
+      <Navbar setShowLogin={setShowLogin}/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/order' element={<PlaceOrder/>}/>
+        <Route path='/myorders' element={<MyOrders/>}/>
+        <Route path='/verify' element={<Verify/>}/>
+        <Route path="/about-us" element={<AboutUs />} />
+      </Routes>
+    </div>
+    <Footer/>
+
+    </>
+    
+  )
+}
+
+export default App
